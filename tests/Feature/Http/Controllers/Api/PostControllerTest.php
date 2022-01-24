766d16dd8be95ef661345a2fc20deedc9af22842
JoelGutierrezN <al221910321@gmail.com>
2022-01-24 11:52:09 -0600

    metodo show

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Post;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostControllerTest extends TestCase
 {
@@ -39,5 +40,25 @@ class PostControllerTest extends TestCase
 
         $response->assertStatus(422)
             ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show(){
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure([
+            'id',
+            'title',
+            'created_at',
+            'updated_at'
+        ])
+        ->assertJson(['title' => $post->title])
+        ->assertStatus(200);
+    }
+
+    public function test_404_show(){
+        $response = $this->json('GET', "/api/posts/1000");
+        $response->assertStatus(404);
     }
 }
